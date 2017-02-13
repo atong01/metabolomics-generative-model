1c19690205c72cec3930e2b4a0fcabe5bd82d14b
atong01 <alexanderytong@gmail.com>
2017-02-13 02:04:04 -0500

    add poisson model with real data

--- poisson/pmodel.py
+++ poisson/pmodel.py
@@ -2,24 +2,28 @@ from pymc import Gamma, Poisson, InverseGamma
 import pymc
 import numpy as np
 import gen
-alpha = 2
-beta = 1
+import read
 
-pathways = gen.pathways()
-features = gen.features_dict()
-detected = gen.detected_features()
-evidence = gen.evidence()
-ap =  {p.name : Gamma('p_' + p.name, p.rate, beta) for p in pathways}
-#bmp = [Gamma('b_{' + str(i) + '}', 1, ap[i]) for i in range(3)]
-bmp = {p.name : {feat : Gamma('b_{' + p.name + ',' + str(feat) + '}', ap[p.name],1) for feat in p.mets} for p in pathways}
-print bmp
-#g_bmp = {feat : Poisson('g_' + str(feat), sum([bmp[pname][feat] for pname in pathways])) for feat, pathways in features.iteritems()}
+data_path = '../data/'
+observation_file = data_path + 'HilNeg 0324 -- Data.csv'
+cofactors = read.get_cofactors(data_path + 'cofactors')
+path_dict = read.get_model(data_path + 'model2.csv', cofactors = cofactors)
+pathways = path_dict.keys()
+features = read.get_metabolites(path_dict)
+evidence = read.metlin(observation_file)
+evidence |= read.hmdb(observation_file)
+evidence -= cofactors
+features -= cofactors
+evidence &= features
+reverse_path_dict = read.reverse_dict(path_dict)
+rate_prior = 0.5
+print evidence
+
+ap =  {p : Gamma('p_' + p, rate_prior, 1) for p in pathways}
+bmp = {p : {feat : Gamma('b_{' + p + ',' + feat + '}', ap[p],1) for feat in path_dict[p]} for p in pathways}
 g_bmp = {}
-for feat, pathways in features.iteritems():
-    if detected(feat):
-        print feat, "was detected"
-        g_bmp[feat] = Poisson('g_' + str(feat), sum([bmp[pname][feat] for pname in pathways]), value = 1, observed = True)
+for feat, pathways in reverse_path_dict.iteritems():
+    if feat in evidence:
+        g_bmp[feat] = Poisson('g_' + feat, sum([bmp[pname][feat] for pname in pathways]), value = 1, observed = True)
     else:
-        print feat, "was not detected"
-        g_bmp[feat] = Poisson('g_' + str(feat), sum([bmp[pname][feat] for pname in pathways]))
-print g_bmp
+        g_bmp[feat] = Poisson('g_' + feat, sum([bmp[pname][feat] for pname in pathways]))
