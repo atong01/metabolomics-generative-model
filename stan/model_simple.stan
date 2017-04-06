5f0c2fc50abda7b833ffe87f9e82a9d4ac6fcd22
atong01 <alexanderytong@gmail.com>
2017-04-05 23:56:58 -0400

    Initial Stan Sampling

--- stan/model_simple.stan
+++ stan/model_simple.stan
@@ -6,15 +6,15 @@ data {
   matrix<lower=0.0,upper=1.0>[P,M] metabolite_mask;
   // y[m] = 1 if metabolite m has been observed
   int<lower=0,upper=1> y[M];
+  real<lower=0.0> tiny;
 }
 parameters {
-  real<lower=0.0,upper=1.0> eps;
+  vector<lower=0.0,upper=1.0>[M] eps;
   vector<lower=0.0>[P] a;
-  matrix<lower=0.0>[P,M] b;
 }
 transformed parameters {
   row_vector<lower=0.0>[M] beta;
-  beta <- rep_row_vector(1.0, P) * (b .* metabolite_mask);
+  beta = a' * metabolite_mask;
 }
 model {
   // base probability of detection
@@ -22,13 +22,11 @@ model {
   // activity rate for each pathway
   a ~ gamma(rate_prior, 1.0);
   for (m in 1:M)
-    // per-pathway activity rate for metabolite m
-    b[:,m] ~ gamma(a, 1.0);
-  for (m in 1:M)
     if (y[m]==1)
+      // p(y[m]=1 | beta[m]) = 
       // log probability of detection
-      target += log(eps) - 0.5*beta[m] + log(1-exp(-beta[m]));
+      target += log(eps[m]) - 0.5*beta[m] + log(1-exp(-beta[m]) + tiny);
     else 
       // log probability of detection failure
-      target += log((1-eps) + eps*exp(-beta[m]));
+      target += log((1-eps[m]) + eps[m]*exp(-beta[m]));
 }
---
+++ stan/read.py
@@ -0,0 +1,168 @@
+"""
+Provides helper functions for reading in data
+"""
+import numpy as np
+import csv
+from collections import defaultdict
+
+def get_model(model_path, keep_full_path=False, cofactors = set()):
+    """ returns (dict) pathway : set(compounds) """
+    with open(model_path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+        data = np.array([row for row in reader])
+    path_dict = defaultdict(set)
+    for line in data[1:]:
+        path = line[0]
+        compounds = set(line[5].split()).union(set(line[6].split()))
+        compounds -= cofactors
+        path_dict[path].update(compounds)
+    if not keep_full_path:
+        del path_dict['cge01100']
+    return path_dict
+
+def reverse_dict(d):
+    to_return = defaultdict(list)
+    for k, v in d.items():
+        for c in v:
+            to_return[c].append(k)
+    return to_return
+
+def get_metabolites(path_dict):
+    mets = set()
+    for p in path_dict.values():
+        mets |= p
+    return mets
+
+def get_column_set(path, col):
+    """ Gets set of comma separated strings in column """
+    if type(path) == list:
+        return reduce(lambda x, y: x | y, [get_column_set(p, col) for p in path])
+    to_return = set()
+    data = []
+    with open(path, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data.extend([c.strip() for c in row[col].split(',')])
+    for d in data[3:]:
+        to_return.add(d)
+    to_return.remove('')
+    return to_return
+
+def get_cofactors(path):
+    with open(path, 'r') as cof_file:
+        reader = csv.reader(cof_file)
+        cofactors = set(["C" + row[0].zfill(5) for row in reader])
+    return cofactors
+
+hmdb    = lambda path: get_column_set(path, 5)
+metlin  = lambda path: get_column_set(path, 7)
+metfrag = lambda path: get_column_set(path, 9)
+
+def or_scores(scores):
+    to_return = defaultdict(float)
+    for c, score_list in scores.items():
+        tot = 1
+        for s in score_list:
+            tot *= (1 - float(s))
+        to_return[c] = 1 - tot
+    return to_return
+
+def metfrag_with_scores(path, keep_zero_scores = True):
+    """
+        Returns dict: Compound --> Score
+        Score is calculated as 1 - prod(1 - p) for p in scores
+    """
+    scores = defaultdict(list)
+    to_return = defaultdict(float)
+    if type(path) == list:
+        dicts = [metfrag_with_scores(p, keep_zero_scores) for p in path]
+        for d in dicts:
+            for k, v in d.items():
+                scores[k].append(v)
+        return or_scores(scores)
+    with open(path, 'r') as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
+            if i < 3:
+                continue
+            for c, score in zip(*(row[9].split(','), row[10].split(','))):
+                scores[c].append(score.strip())
+        del scores['']
+    to_return = or_scores(scores)
+
+    if not keep_zero_scores:
+        to_return = dict((k,v) for k, v in to_return.items() if v != 0)
+    return to_return
+
+def clean_sets():
+    """ Gets cleaned sets of evidence and metfrag """
+    data_path = '../data/'
+    observation_file = data_path + 'HilNeg 0324 -- Data.csv'
+    cofactors = get_cofactors(data_path + 'cofactors')
+    path_dict = get_model(data_path + 'model2.csv', cofactors = cofactors)
+    pathways = path_dict.keys()
+    features = get_metabolites(path_dict)
+    evidence = metlin(observation_file)
+    evidence |= hmdb(observation_file)
+    evidence -= cofactors
+    features -= cofactors
+    evidence &= features
+    reverse_path_dict = reverse_dict(path_dict)
+    mf = metfrag(observation_file)
+    metfrag_evidence = dict_of_set(metfrag_with_scores(observation_file, keep_zero_scores = False), mf & features - cofactors - evidence)
+    return evidence, metfrag_evidence
+
+def get_all_sets():
+    data_path = '../data/'
+    observation_file = data_path + 'HilNeg 0324 -- Data.csv'
+    cofactors = get_cofactors(data_path + 'cofactors')
+    path_dict = get_model(data_path + 'model2.csv', cofactors = cofactors)
+    pathways = path_dict.keys()
+    features = get_metabolites(path_dict)
+    evidence = metlin(observation_file)
+    evidence |= hmdb(observation_file)
+    evidence -= cofactors
+    features -= cofactors
+    evidence &= features
+    reverse_path_dict = reverse_dict(path_dict)
+    mf = metfrag(observation_file)
+    metfrag_evidence = dict_of_set(metfrag_with_scores(observation_file, keep_zero_scores = False), mf & features - cofactors - evidence)
+    evidence = {e : 1 for e in evidence}
+    return pathways, features, path_dict, reverse_path_dict, evidence, metfrag_evidence
+
+def dict_of_set(d, s):
+    """ Returns a dict with keys in set """
+    return dict((k,v) for k, v in d.items() if k in s)
+
+def get_matrix_form(include_soft_evidence = True):
+    pathways, features, path_dict, reverse_path_dict, evidence, metfrag_evidence = get_all_sets()
+    pathways = np.array(list(pathways))
+    metabolites = np.array(list(features))
+    rev_paths = {p:i for i,p in enumerate(pathways)}
+    rev_mets = {m:i for i,m in enumerate(metabolites)}
+    P = pathways.shape[0]
+    M = metabolites.shape[0]
+
+    mask = np.zeros((P,M))
+    for p,ms in path_dict.items():
+        for m in ms:
+            mask[rev_paths[p],rev_mets[m]] = 1
+    evidence_vec = np.zeros((M,))
+    for e,v in evidence.items():
+        evidence_vec[rev_mets[e]] = v
+    if include_soft_evidence:
+        for e,v in metfrag_evidence.items():
+            evidence_vec[rev_mets[e]] = v
+    else:
+        evidence_vec = evidence_vec.astype(np.int32)
+    return rev_paths, rev_mets, mask.astype(np.int32), evidence_vec
+
+if __name__ == "__main__":
+    data = '../data/HilNeg 0324 -- Data.csv'
+    d = get_model('../data/model2.csv')
+    #mets = get_metabolites(d)
+    rev_paths, rev_mets, mask, evidence = get_matrix_form(include_soft_evidence = False)
+    print (np.sum(mask))
+    #print len(metfrag_with_scores(data))
+    #print len(metfrag_with_scores(data, keep_zero_scores = False))
+    #print sorted(list((hmdb(data) | metlin(data)) & mets))
